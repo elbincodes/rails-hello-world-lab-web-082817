93b10bc304f0738327482b8afd03d22d9c0b5af4
elbincodes <elbincodes@gmail.com>
2017-09-22 12:22:27 -0400

    Done.

---
+++ app/controllers/helloworld_controller.rb
@@ -0,0 +1,12 @@
+class HelloworldController < ApplicationController
+
+  # def hello_world
+  # end
+
+
+
+#when did I need to remove the above method? h what allowed for this? is it the config / routes?
+
+# also, why is the learn test not passing but this is appearing on my site?
+
+end
